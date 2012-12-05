4c4dc2a280603886917eb352e65ad85e4e830c61
Hernán Meydac Jean <hernan.meydac@southworks.net>
2012-12-04 21:11:50 -0300

    - Removed basic themes. - Added custom styles. - Added images. - Revamped handlebars templates.

--- Lampadas/Lampadas.Web/App_Start/BundleConfig.cs
+++ Lampadas/Lampadas.Web/App_Start/BundleConfig.cs
@@ -29,23 +29,7 @@
                         "~/Scripts/Libs/modernizr-*"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/site.css",
-                "~/Content/bootstrap.css",
-                "~/Content/bootstrap-responsive.css"));
-
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+                "~/Content/themes/office17/site.css"));
 
             RegisterEmber(bundles);
         }
