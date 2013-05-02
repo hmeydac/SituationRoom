92677799d68582cd85afca62132dafb90b87cd27
Hernán Meydac Jean <hernan.meydac@southworks.net>
2013-05-01 22:01:13 -0300

    - Added homeworld.

---
+++ Homeworld/Homeworld/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿namespace Homeworld.Controllers
+{
+    using System.Web.Mvc;
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+
+        public ActionResult Concept()
+        {
+            return this.View();
+        }
+    }
+}
---
+++ Homeworld/Homeworld/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Homeworld.MvcApplication" Language="C#" %>
